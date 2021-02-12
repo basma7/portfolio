ae2a388dbe99336acef8f29ee054f3fe96e91f7d
ibtissem <rezig.ibtissam@gmail.com>
2021-02-12 13:48:22 +0100

    Add project page

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -5,7 +5,7 @@ import { Navbar, Nav } from 'react-bootstrap'
 export default function Header() {
   return (
     <Navbar>
-      <Navbar.Brand href="#home">Ibtissem rezig</Navbar.Brand>
+      <Navbar.Brand href="">Ibtissem rezig</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
         <Nav>
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,11 +1,55 @@
 import React from 'react'
 import { Col } from 'react-bootstrap'
+import { Model } from '../../ConstIndex'
 import './Project.scss'
 
 export default function Project() {
   return (
     <div className="project">
-      <Col className=''></Col>
+      <div className="d-flex d-row justify-content-center">
+        <img alt="pjt-img" src={Model} />
+      </div>
+      <div className="d-flex d-row justify-content-center dsc-prj">
+          <Col lg={3}>
+            <p className='ttl-txt'>The project</p>
+            <div className="und-line"></div>
+          </Col>
+          <Col lg={6}>
+            <p className="dsc-txt">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+              took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+              but also the leap into electronic typesetting, remaining essentially unchanged. 
+              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,</p>
+          </Col>
+        </div>
+
+        <div className="d-flex d-row justify-content-center dsc-prj">
+          <Col lg={3}>
+            <p className='ttl-txt'>The context</p>
+            <div className="und-line"></div>
+          </Col>
+          <Col lg={6}>
+            <p className="dsc-txt">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+              took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+              but also the leap into electronic typesetting, remaining essentially unchanged. 
+              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,</p>
+          </Col>
+        </div>
+
+        <div className="d-flex d-row justify-content-center dsc-prj">
+          <Col lg={3}>
+            <p className='ttl-txt'>The project</p>
+            <div className="und-line"></div>
+          </Col>
+          <Col lg={6}>
+            <p className="dsc-txt">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+              took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+              but also the leap into electronic typesetting, remaining essentially unchanged. 
+              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,</p>
+          </Col>
+        </div>
     </div>
   )
 }
